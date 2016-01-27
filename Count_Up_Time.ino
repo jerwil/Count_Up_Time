570e0661d9f3c5299a192b6ab892c5336125f1fd
Jeremy Wilson <jerwil@gmail.com>
2016-01-26 22:47:32 -0600

    Produced circuit board, basic time calculations working
    Rotary encoder not yet working. Need to troubleshoot

--- Count_Up_Time.ino
+++ Count_Up_Time.ino
@@ -16,6 +16,17 @@ LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars
 
 RTC_DS1307 rtc;
 
+const int pin_A = 6;  // Rotary Encoder Pin A
+const int pin_B = 7;  // Rotary Encoder Pin B
+
+int clockwise = 0;
+int counterclockwise = 0;
+int mode = 0;
+
+unsigned char encoder_A;
+unsigned char encoder_B;
+unsigned char encoder_A_prev=0;
+
 void setup () {
   Serial.begin(57600);
 #ifdef AVR
@@ -37,67 +48,122 @@ void setup () {
  
   // Print a message to the LCD.
   lcd.backlight();
-  
+  pinMode(pin_A, INPUT);
+  pinMode(pin_B, INPUT);
 
 }
 
 void loop () {
+
+    clockwise = 0;
+    counterclockwise = 0;
+    encoder_A = digitalRead(pin_A);    // Read encoder pins
+    encoder_B = digitalRead(pin_B);   
+    if((!encoder_A) && (encoder_A_prev)){
+      // A has gone from high to low 
+      if(encoder_B) {
+        // B is high so clockwise
+        // increase the brightness multiplier, dont go over 25
+        clockwise = 1;
+        mode += 1;            
+      }   
+      else {
+        // B is low so counter-clockwise      
+        // decrease the brightness, dont go below 0
+         counterclockwise = 1;
+       mode -= 1;        
+      } 
+    }
+//    if (mode < 0){mode = 4;}
+//    else if (mode > 4){mode = 0;}
+  
     //rtc.adjust(DateTime(__DATE__, __TIME__));
     DateTime now = rtc.now();
+    DateTime aniv = (1422126000);
     
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(' ');
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");
-    lcd.clear();
-    String stringOne =  String(now.hour(), DEC) + ':' + String(now.minute(), DEC) + ':' + String(now.second(), DEC);
-    printstring(stringOne);
+//    Serial.print(now.year(), DEC);
+//    Serial.print('/');
+//    Serial.print(now.month(), DEC);
+//    Serial.print('/');
+//    Serial.print(now.day(), DEC);
+//    Serial.print(' ');
+//    Serial.print(now.hour(), DEC);
+//    Serial.print(':');
+//    Serial.print(now.minute(), DEC);
+//    Serial.print(':');
+//    Serial.print(now.second(), DEC);
+//    Serial.println();
+//    
+//    Serial.print(" since midnight 1/1/1970 = ");
+//    Serial.print(now.unixtime());
+//    Serial.print("s = ");
+//    Serial.print(now.unixtime() / 86400L);
+//    Serial.println("d");
+//    Serial.println("Unixtime of anniversary");
+//    Serial.println(aniv.unixtime());
+//
+//    Serial.print(aniv.year(), DEC);
+//    Serial.print('/');
+//    Serial.print(aniv.month(), DEC);
+//    Serial.print('/');
+//    Serial.print(aniv.day(), DEC);
+//    Serial.print(' ');
+//    Serial.print(aniv.hour(), DEC);
+//    Serial.print(':');
+//    Serial.print(aniv.minute(), DEC);
+//    Serial.print(':');
+//    Serial.print(aniv.second(), DEC);
+//    Serial.println();
 
+    long SecondsSince = now.unixtime() - aniv.unixtime();
+    long MinutesSince = SecondsSince/60;
+    long HoursSince = MinutesSince/60;
+    long DaysSince = HoursSince/24;
+    long MonthsSince = (now.year()-aniv.year())*12+(now.month()-aniv.month());
+    //lcd.clear();
+//    String stringOne =  String(now.hour(), DEC) + ':' + String(now.minute(), DEC) + ':' + String(now.second(), DEC);
+//    printstring(stringOne,0);
+//    printstring("The time",1);
+
+  if (mode == 0){
+      printstring(String(SecondsSince),0);
+      printstring("Seconds",1);
+  }
+  else if (mode == 1){
+      printstring(String(MinutesSince),0);
+      printstring("Minutes",1);
+  }
+  else if (mode == 2){
+      printstring(String(HoursSince),0);
+      printstring("Hours",1);
+  }
+  else if (mode == 3){
+      printstring(String(DaysSince),0);
+      printstring("Days",1);
+  }
+  else if (mode == 4){
+      printstring(String(MonthsSince),0);
+      printstring("Months",1);
+  }
+  else {printstring("Error!",0);  
+  }
     
     // calculate a date which is 7 days and 30 seconds into the future
     DateTime future (now.unixtime() + 7 * 86400L + 30);
     
-    Serial.print(" now + 7d + 30s: ");
-    Serial.print(future.year(), DEC);
-    Serial.print('/');
-    Serial.print(future.month(), DEC);
-    Serial.print('/');
-    Serial.print(future.day(), DEC);
-    Serial.print(' ');
-    Serial.print(future.hour(), DEC);
-    Serial.print(':');
-    Serial.print(future.minute(), DEC);
-    Serial.print(':');
-    Serial.print(future.second(), DEC);
-    Serial.println();
-    
-    Serial.println();
+    Serial.println(mode);
     //lcd.backlight();
-    delay(1000);
+    //delay(1000);
     //lcd.noBacklight();
     
 }
 
-void printstring(String stringtoprint) {
+void printstring(String stringtoprint, int row) {
   int stringlength = stringtoprint.length();
   char StringChar[stringlength + 1]; 
   stringtoprint.toCharArray(StringChar, stringlength + 1);
   for (int i = 0; i < stringlength; i++){
-  lcd.setCursor(i, 0);
+  lcd.setCursor(i, row);
   lcd.print(StringChar[i]);
 }
 }
